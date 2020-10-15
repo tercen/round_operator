ad250b4b69a8f2740f8aa8decfdf26af393b4557
agouy <contact@agouy.com>
2020-10-15 17:25:17 +0000

    first version

--- main.R
+++ main.R
@@ -1,9 +1,22 @@
 library(tercen)
 library(dplyr)
 
+method <- "round"
+if(!is.null(ctx$op.value('method'))) method <- ctx$op.value('method')
+digits <- 0
+if(!is.null(ctx$op.value('digits'))) digits <- as.numeric(ctx$op.value('digits'))
+
+rnd <- function(y, method, digits) {
+  if(method %in% c("round", "signif")) {
+    out <- sapply(y, method, digits = digits)
+  } else {
+    out <- sapply(y, method)
+  }
+  return(out)
+}
+
 (ctx = tercenCtx())  %>% 
   select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  mutate(rounded = rnd(.y, method, digits)) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,25 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "d330322c43363eb4f9b27738ef0042b9")
+options("tercen.stepId"     = "1ae42627-e9ce-4d9f-9797-8700adfd7718")
 
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
+method <- "round"
+if(!is.null(ctx$op.value('method'))) method <- ctx$op.value('method')
+digits <- 0
+if(!is.null(ctx$op.value('digits'))) digits <- as.numeric(ctx$op.value('digits'))
+
+rnd <- function(y, method, digits) {
+  if(method %in% c("round", "signif")) {
+    out <- sapply(y, method, digits = digits)
+  } else {
+    out <- sapply(y, method)
+  }  
+  return(out)
+}
 
 (ctx = tercenCtx())  %>% 
   select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  mutate(rounded = rnd(.y, method, digits)) %>%
   ctx$addNamespace() %>%
   ctx$save()
