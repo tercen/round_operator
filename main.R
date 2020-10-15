138c7a86a4952c699ff4161e232f1f166183e858
agouy <contact@agouy.com>
2020-10-15 17:29:26 +0000

    bug fix

--- main.R
+++ main.R
@@ -1,11 +1,6 @@
 library(tercen)
 library(dplyr)
 
-method <- "round"
-if(!is.null(ctx$op.value('method'))) method <- ctx$op.value('method')
-digits <- 0
-if(!is.null(ctx$op.value('digits'))) digits <- as.numeric(ctx$op.value('digits'))
-
 rnd <- function(y, method, digits) {
   if(method %in% c("round", "signif")) {
     out <- sapply(y, method, digits = digits)
@@ -15,7 +10,14 @@ rnd <- function(y, method, digits) {
   return(out)
 }
 
-(ctx = tercenCtx())  %>% 
+ctx <- tercenCtx()
+
+method <- "round"
+if(!is.null(ctx$op.value('method'))) method <- ctx$op.value('method')
+digits <- 0
+if(!is.null(ctx$op.value('digits'))) digits <- as.numeric(ctx$op.value('digits'))
+
+ctx %>% 
   select(.y, .ci, .ri) %>% 
   mutate(rounded = rnd(.y, method, digits)) %>%
   ctx$addNamespace() %>%
